6aa848d0f7e3a00c5a010efec677994015383bae
EhsanFox <ehsan8184@gmail.com>
2022-04-01 14:41:27 +0430

    Some cleanups

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import App from "./App";
 import { RouteHandlers } from "./Components";
